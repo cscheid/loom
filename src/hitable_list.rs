af4076d10067c98dd6c44c6e0ebe980063c5da3e
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-22 21:06:11 -0700

    hitable list

---
+++ src/hitable_list.rs
@@ -0,0 +1,31 @@
+use hitable::*;
+use ray::Ray;
+
+pub struct HitableList {
+    pub v: Vec<Box<Hitable>>
+}
+
+impl HitableList {
+    pub fn new(v: Vec<Box<Hitable>>) -> HitableList {
+        HitableList { v: v }
+    }
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_record = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+        for hitable_box in self.v.iter() {
+            let hitable = &*hitable_box;
+            if hitable.hit(r, t_min, closest_so_far, &mut temp_record) {
+                hit_anything = true;
+                closest_so_far = temp_record.t;
+                rec.t = temp_record.t;
+                rec.p = temp_record.p;
+                rec.normal = temp_record.normal;
+            }
+        }
+        return hit_anything;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ mod vector;
 mod ray;
 mod hitable;
 mod sphere;
+mod hitable_list;
 
 use vector::Vec3;
 use ray::Ray;
-use hitable::HitRecord;
-use sphere::Sphere;
-use hitable::Hitable;
+use hitable::*;
+use sphere::*;
+use hitable::*;
+use hitable_list::*;
 
-fn color(ray: &Ray) -> Vec3
+fn color(ray: &Ray, world: &Hitable) -> Vec3
 {
     let mut r = HitRecord::new();
-    let s = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
-    if s.hit(ray, 0.0, 1e20, &mut r) {
+
+    if world.hit(ray, 0.0, 1e20, &mut r) {
         return 0.5 * (r.normal + Vec3::new(1.0, 1.0, 1.0));
     } else {
         let unit_direction = vector::unit_vector(&ray.direction());
@@ -31,14 +33,19 @@ fn write_image() {
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+
+    let s1 = Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5));
+    let s2 = Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0));
     
+    let world = HitableList::new(vec![s1, s2]);
+
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = (i as f64) / (nx as f64);
             let v = (j as f64) / (ny as f64);
             let r = Ray::new(origin,
                              lower_left_corner + u*horizontal + v*vertical);
-            let col = color(&r);
+            let col = color(&r, &world);
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
