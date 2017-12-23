0b68d6013d01f08ae7f49e8267cdbf57c0ecce66
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-22 18:55:19 -0700

    minimal vec3 class

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use vector::V;
-
 mod vector;
+use vector::Vec3;
+
+fn write_ppm() {
+    let nx = 200;
+    let ny = 100;
+    println!("P3\n{} {}\n255", nx, ny);
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let col = Vec3::new((i as f64) / (nx as f64),
+                                (j as f64) / (ny as f64),
+                                0.2);
+            let ir = (255.99 * col[0]) as i32;
+            let ig = (255.99 * col[1]) as i32;
+            let ib = (255.99 * col[2]) as i32;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
 
 fn main() {
-    let v1 = V::new(1.0, 0.0, 0.0);
-    let v2 = V::new(0.0, 1.0, 0.0);
-    println!("v1 is {:?}", v1);
-    println!("v1 + v2 is {:?}", &v1 + &v2); // this is a little goofy, but ok
-    println!("v1 - v2 is {:?}", &v1 - &v2);
-    println!("v1 * v1 is {:?}", &v1 * &v2);
+    write_ppm();
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,45 +1,178 @@
-// V for Vector, since Vec is already taken :(
-
 use std::ops;
 
 #[derive(Debug, Copy, Clone)]
-pub struct V {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64
+pub struct Vec3 {
+    e: [f64; 3]
 }
 
-impl V {
-    pub fn new(x: f64, y: f64, z: f64) -> V {
-        V { x: x, y: y, z: z }
+impl Vec3 {
+    #[inline]
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { e: [x, y, z] }
     }
+
+    #[inline]
+    fn x(&self) -> f64 { self.e[0] }
+    #[inline]
+    fn y(&self) -> f64 { self.e[1] }
+    #[inline]
+    fn z(&self) -> f64 { self.e[2] }
+    #[inline]
+
+    #[inline]
+    fn r(&self) -> f64 { self.e[0] }
+    #[inline]
+    fn g(&self) -> f64 { self.e[1] }
+    #[inline]
+    fn b(&self) -> f64 { self.e[2] }
+
+    #[inline]
+    pub fn dot(&self, _rhs: &Vec3) -> f64 {
+        self.e[0] * _rhs.e[0] + self.e[1] * _rhs.e[1] + self.e[2] * _rhs.e[2]
+    }
+
+    #[inline]
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    #[inline]
+    pub fn length_squared(&self) -> f64 {
+        self.dot(&self)
+    }
+    
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
-impl<'a, 'b> ops::Add<&'b V> for &'a V {
-    type Output = V;
-
-    fn add(self, _rhs: &'b V) -> V {
-        V { x: self.x + _rhs.x,
-            y: self.y + _rhs.y,
-            z: self.z + _rhs.z }
-    }
-}
-
-impl<'a, 'b> ops::Sub<&'b V> for &'a V {
-    type Output = V;
-
-    fn sub(self, _rhs: &'b V) -> V {
-        V { x: self.x - _rhs.x,
-            y: self.y - _rhs.y,
-            z: self.z - _rhs.z }
-    }
-}
-
-impl<'a, 'b> ops::Mul<&'b V> for &'a V {
+impl ops::Index<usize> for Vec3 {
     type Output = f64;
-    fn mul(self, _rhs: &'b V) -> f64 {
-        self.x * _rhs.x + self.y * _rhs.y + self.z * _rhs.z
+    fn index<'a>(&'a self, index: usize) -> &'a f64 {
+        &self.e[index]
     }
+}
+
+impl ops::IndexMut<usize> for Vec3 {
+    fn index_mut<'a>(&'a mut self, index: usize) -> &'a mut f64 {
+        &mut self.e[index]
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [self.e[0] + _rhs.e[0],
+                   self.e[1] + _rhs.e[1],
+                   self.e[2] + _rhs.e[2]] }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [self.e[0] - _rhs.e[0],
+                   self.e[1] - _rhs.e[1],
+                   self.e[2] - _rhs.e[2]] }
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [self.e[0] * _rhs.e[0],
+                   self.e[1] * _rhs.e[1],
+                   self.e[2] * _rhs.e[2]] }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, _rhs: f64) -> Vec3 {
+        Vec3 { e: [self.e[0] * _rhs,
+                   self.e[1] * _rhs,
+                   self.e[2] * _rhs] }
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [_rhs.e[0] * self,
+                   _rhs.e[1] * self,
+                   _rhs.e[2] * self] }
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn div(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [self.e[0] / _rhs.e[0],
+                   self.e[1] / _rhs.e[1],
+                   self.e[2] / _rhs.e[2]] }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, _rhs: f64) -> Vec3 {
+        Vec3 { e: [self.e[0] / _rhs,
+                   self.e[1] / _rhs,
+                   self.e[2] / _rhs] }
+    }
+}
+
+impl ops::Div<Vec3> for f64 {
+    type Output = Vec3;
+    fn div(self, _rhs: Vec3) -> Vec3 {
+        Vec3 { e: [self / _rhs.e[0],
+                   self / _rhs.e[1],
+                   self / _rhs.e[2]] }
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3 {e: [-self.e[0],
+                  -self.e[1],
+                  -self.e[2]] }
+    }
+}
+
+// pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3
+// {
+//     Vec3::new(v2.e[1]
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+#[test]
+fn it_doesnt_smoke() {
+    let mut v1 = Vec3::new(1.0, 0.0, 0.0);
+    let mut v2 = Vec3::new(0.0, 1.0, 0.0);
+    
+    println!("v1 is {:?}", v1);
+    println!("v1 + v2 is {:?}", v1 + v2); // this is a little goofy, but ok
+    println!("v1 - v2 is {:?}", v1 - v2);
+    println!("v1 * v1 is {:?}", v1 * v1);
+
+    println!("v1.dot(v2) is {:?}", v1.dot(&v2));
+
+    println!("v1 * v2 is {:?}", v1 * v2);
+    println!("v2 * 2 is {:?}", v2 * 2.0);
+    println!("2 * v2 is {:?}", 2.0 * v2);
+
+    println!("v1 / v2 is {:?}", v1 / v2);
+    println!("v2 / 2 is {:?}", v2 / 2.0);
+    println!("2 / v2 is {:?}", 2.0 / v2);
+
+    println!("v2[0] is {:?}", &v2[0]);
+
+    (&mut v2)[0] = 5.0;
+
+    println!("v2[0] is {:?}", &v2[0]);
+    println!("-v2 is {:?}", -v2);
+    println!("v2.length() is {:?}", v2.length());
+    println!("v2.length_squared() is {:?}", v2.length_squared());
 }
