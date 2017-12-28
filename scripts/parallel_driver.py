c3e97dc48d8945ed97ac51fb50a656b385a56ed5
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-28 13:20:58 -0700

    write temp files at configurable interval

--- scripts/parallel_driver.py
+++ scripts/parallel_driver.py
@@ -8,7 +8,8 @@ n_procs = int(sys.argv[1])
 scene = sys.argv[2]
 height = sys.argv[3]
 samples = sys.argv[4]
-out_file = sys.argv[5]
+interval = sys.argv[5]
+out_file = sys.argv[6]
 
 def render(proc_id):
     print("Rendering %s" % proc_id)
@@ -16,6 +17,7 @@ def render(proc_id):
                     "-i", scene,
                     "-s", samples,
                     "-h", height,
+                    "-t", interval,
                     "-p",
                     "-o", "%s-%s" % (out_file, proc_id)])
 
